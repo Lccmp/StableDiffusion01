f5c6d253b105f80fa68ff2c69bfca3149cbec295
李捷媮 <jenniful991@gmail.com>
2025-06-20 22:47:53 +0800

    Delete website directory

--- website/static/js/base.js
+++
@@ -1,8 +0,0 @@
-// 側邊選單開啟和關閉的函式
-function openMenu() {
-    document.getElementById("sideMenu").classList.add("open");
-}
-
-function closeMenu() {
-    document.getElementById("sideMenu").classList.remove("open");
-}
